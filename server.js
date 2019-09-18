5729303e0143d9df50ced0bf01820b7226a3a849
Ali Farhan <f1b212008@gmail.com>
2019-09-18 08:30:20 +0700

    url encode server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser')
 
 
 const app = express();
-// app.use(bodyParser.urlencoded({
-//     extended: false
-// }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // parse application/json
 app.use(bodyParser.json())
